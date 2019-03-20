a49e7790dfab746e3f99275d114905f7acde9e85
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-20 21:42:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,6 +1,6 @@
 def instantiate_new_array
  my_new_array = []
  end
- def array_with_two_elements(array,elements)
+ def array_with_two_elements(array,element)
    my_two_array = ["pinky", "brain"]
  end
